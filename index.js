c6fe4d9de63501fbaa05e838cd8036669fcbd833
linegj <linekbv@gmail.com>
2025-04-02 12:16:22 +0200

    cirkel flytter sig

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const dodger = document.getElementById("dodger");
+const point = document.getElementById("point");
 
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
@@ -9,6 +10,7 @@ function moveDodgerLeft() {
     if (left > 0) { // dodger går ikke ud over venstre kant
         dodger.style.left = `${left - 1}px`; // 1px til venstre
         playSoundOnMovement();
+        checkCollision();
     }
 }
 
@@ -19,6 +21,7 @@ function moveDodgerRight() {
     if (left < 360) { // container = 400px og dodgeren= 40px (dodger går ikke ud over højre kant)
         dodger.style.left = `${left + 1}px`; // 1px til højre
         playSoundOnMovement();
+        checkCollision();
     }
 }
 
@@ -29,6 +32,7 @@ function moveDodgerUp() {
     if (bottom < 360) { 
         dodger.style.bottom = `${bottom + 1}px`; // 1px opad
         playSoundOnMovement();
+        checkCollision();
     }
 }
 
@@ -39,6 +43,7 @@ function moveDodgerDown() {
     if (bottom > 0) {
       dodger.style.bottom = `${bottom - 1}px`; // 1px nedad
       playSoundOnMovement();
+      checkCollision();
     }
 }
   
@@ -66,4 +71,40 @@ const movementSound = document.getElementById("movementSound");
 function playSoundOnMovement() {
     movementSound.currentTime = 0; // starter forfra
     movementSound.play(); 
+}
+
+//cirkel placering
+function getRandomPosition() {
+    const gameArea = document.getElementById("game").getBoundingClientRect();
+    const maxX = gameArea.width - 20; // 20px bred
+    const maxY = gameArea.height - 20; // 20px høj
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    return { x: randomX, y: randomY };
+}
+
+//Random placering af cirkel
+function movePointRandomly() {
+    const newPosition = getRandomPosition();
+    point.style.left = `${newPosition.x}px`;
+    point.style.top = `${newPosition.y}px`;
+    point.style.display = "block"; // Synlig
+}
+
+
+function checkCollision() {
+    const dodgerRect = dodger.getBoundingClientRect();
+    const pointRect = point.getBoundingClientRect();
+
+    if (
+        dodgerRect.left < pointRect.right &&
+        dodgerRect.right > pointRect.left &&
+        dodgerRect.top < pointRect.bottom &&
+        dodgerRect.bottom > pointRect.top
+    ) {
+        point.style.display = "none"; // usynlig
+        setTimeout(movePointRandomly, 1000); // Flyt cirklen efter 1 sek
+    }
 }
