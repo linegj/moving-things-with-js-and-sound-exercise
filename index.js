2de24d6fe287d00ee6f829b980442b312a143cee
linegj <linekbv@gmail.com>
2025-04-02 11:17:46 +0200

    øvelse 1

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+"use strict";
+
+
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function (event) {
+        if (event.key === "ArrowLeft") {
+          const leftNumbers = dodger.style.left.replace("px", "");
+          const left = parseInt(leftNumbers, 10);
+      
+          dodger.style.left = `${left - 1}px`;
+        }
+});
+
+function moveDodgerLeft() {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+      
+        if (left > 0) {
+          dodger.style.left = `${left - 1}px`; // 1px til venstre
+        }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", ""); 
+    const left = parseInt(leftNumbers, 10); 
+  
+    if (left < 360) { // container = 400px og dodgeren= 40px 
+      dodger.style.left = `${left + 1}px`; // 1px til højre
+    }
+  }
+  
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") { // Venstre
+      moveDodgerLeft();
+    }
+  
+    if (e.key === "ArrowRight") { // Højre
+      moveDodgerRight(); 
+    }
+  });
+  
